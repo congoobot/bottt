359535a8d8b3b29a6a70611497cfda2ef2907c59
Glitch (like-seasoned-opossum) <none>
2023-12-19 14:33:33 +0000

    👹🥪 Updated with Glitch

--- events/message.js
+++ events/message.js
@@ -1,21 +1,14 @@
-const Discord = require("discord.js");
-const ayarlar = require('../ayarlar.json');
-let talkedRecently = new Set();
+const { EmbedBuilder } = require("discord.js");
+var config = require("../config.js");
+const client = require("..");
+const prefix = config.prefix;
 
-module.exports = message => {
-  if (talkedRecently.has(message.author.id)) {
-    return;
-  }
-  talkedRecently.add(message.author.id);
-	setTimeout(() => {
-    talkedRecently.delete(message.author.id);
-  }, 2500);
-  let client = message.client;
+client.on("messageCreate", async (message) => {
+  if (!message.guild) return;
   if (message.author.bot) return;
-  if (!message.content.startsWith(ayarlar.prefix)) return;
-  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
-  let params = message.content.split(' ').slice(1);
-  let perms = client.elevation(message);
+  if (!message.content.startsWith(prefix)) return;
+  let command = message.content.split(" ")[0].slice(prefix.length);
+  let params = message.content.split(" ").slice(1);
   let cmd;
   if (client.commands.has(command)) {
     cmd = client.commands.get(command);
@@ -23,8 +16,7 @@ module.exports = message => {
     cmd = client.commands.get(client.aliases.get(command));
   }
   if (cmd) {
-    if (perms < cmd.conf.permLevel) return;
-    cmd.run(client, message, params, perms);
+    cmd.run(client, message, params);
   }
 
-};
+});
